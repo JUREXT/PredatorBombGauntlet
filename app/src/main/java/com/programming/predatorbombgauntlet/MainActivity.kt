85769ab3ba52dbbfcae575e32278ae5e67e31400
Jure Lokovsek <lokovsek.jure@gmail.com>
2024-04-16 23:04:00 +0200

    Created basic shape.

--- app/src/main/java/com/programming/predatorbombgauntlet/MainActivity.kt
+++ app/src/main/java/com/programming/predatorbombgauntlet/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.programming.predatorbombgauntlet
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.programming.predatorbombgauntlet.ui.theme.PredatorBombGauntletTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +40,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SingleCounter()
                 }
             }
         }
@@ -32,17 +48,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun SingleCounter(
+    modifier: Modifier = Modifier
+) {
+    Box(
         modifier = modifier
+            .fillMaxWidth()
+            .padding(32.dp)
+            .border(width = 4.dp, color = Color.Blue, CutCornerShape(200.dp))
+            .graphicsLayer {
+                shape = CutCornerShape(200.dp)
+                clip = true
+            }
+            .background(color = Color.Red)
     )
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+fun PreviewSingleCounter() {
     PredatorBombGauntletTheme {
-        Greeting("Android")
+        SingleCounter()
     }
 }
